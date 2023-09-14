6eb56c8e43662c2cedd5e5371dc9ee2cfce5ec42
Marxu <fuhaojun@52tt.com>
2023-09-14 18:44:33 +0800

    feat: send meg from extension -> webview

--- extension.js
+++ extension.js
@@ -3,6 +3,12 @@ const hgWebView = require('./webview/helloWebview.js')
 
 function activate(context) {
     console.log('active !');
+
+    // 要传递的 content 
+    const webviewContent = {
+        'description': 'Marxu is a good web developer',
+        'slogan':'Support Marxu , learn more !'
+    }
     
     const webViewCommand = vscode.commands.registerCommand('webview.start', () => {
         vscode.window.showInformationMessage('Hello WebVie , I am Marxu !')
@@ -13,12 +19,27 @@ function activate(context) {
             vscode.ViewColumn.One,
             {}
         )
+        // @ts-ignore
         panel.webview.html = hgWebView
+
+        let count = 0
+        const updateWebview = () => {
+            const txt = count ++ % 2  ? 'description' : 'slogan'
+            panel.webview.html = hgWebView(webviewContent[txt])
+        }
+
+        // init
+        updateWebview()
+
+        // timer
+    setInterval(updateWebview, 1000)
+
     });
 
     context.subscriptions.push(webViewCommand);
 }
 
+
 module.exports = {
-    activate
+    activate,
 };
